cc90cb9b40622e3c0cd6e604a3e83c81e3b63186
Itamar Ravid <iravid@iravid.com>
2010-03-22 15:32:10 +0200

    Added an .hgignore, and a prototype for Subscribers

---
+++ subscriber.py
@@ -0,0 +1,9 @@
+class Subscriber(object):
+    def handle(self, event):
+        event_name = event.__class__.__name__
+        event_handler_name = "on%s" % event_name
+
+        event_handler = getattr(self, event_handler_name, None)
+
+        if event_handler:
+            event_handler(self, event)
