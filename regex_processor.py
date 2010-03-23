438a36cfc1ae327e9734ff2a5b44f2546d2d0878
Itamar Ravid <iravid@iravid.com>
2010-03-23 14:18:25 +0200

    A rule-based RegexProcessor entity

---
+++ regex_processor.py
@@ -0,0 +1,35 @@
+import re
+
+from subscriber import Subscriber
+from event import TriggerDataGenerated
+from event_bus import EventBus
+
+class RegexProcessor(Subscriber):
+    rules = {
+    }
+
+    def __init__(self):
+        for pattern, handlerName in self.rules.items():
+            self.rules[pattern] = getattr(self, "on%s" % handlerName)
+
+    def onMUDDataReceived(self, event):
+        responses = self.matchData(event.data)
+
+        event = TriggerDataGenerated()
+        event.data = responses
+
+        EventBus.instance.publish(event)
+
+    def matchData(self, data):
+        responses = []
+
+        for pattern, handler in self.rules.items():
+            match = re.match(pattern, data)
+
+            if match:
+                response = handler(match)
+
+                if response:
+                    responses.append(response)
+
+        return responses
