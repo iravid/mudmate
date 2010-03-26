429f2a6658bd4cc032aa56daae1b7344d4d91052
Itamar Ravid <iravid@iravid.com>
2010-03-26 22:49:42 +0300

    Telnet stuff

--- mud_client.py
+++ mud_client.py
@@ -1,10 +1,11 @@
 from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.conch.telnet import StatefulTelnetProtocol
 
 from event_bus import EventBus
 from event import MUDDataReceived, ControlDataReceived
 from subscriber import Subscriber
 
-class MUDClient(Protocol):
+class MUDClient(StatefulTelnetProtocol):
     def dataReceived(self, data):
         self.factory._dataReceived(data)
 
