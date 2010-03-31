eb562b63487a2c0cf0442ee18abbc7e5c7bdc97a
Itamar Ravid <iravid@iravid.com>
2010-04-01 02:00:23 +0300

    Logging and stuff

--- event_bus.py
+++ event_bus.py
@@ -1,14 +1,20 @@
+import logging
+
 class EventBus(object):
     class _EventBusImpl(object):
         def __init__(self):
             self.subscribers = []
+            self.logger = logging.getLogger("mud_proxy.EventBus")
 
         def subscribe(self, subscriber):
+            self.logger.debug("%s subscribing" % subscriber.__class__.__name__)
             if subscriber not in self.subscribers:
                 self.subscribers.append(subscriber)
 
         def publish(self, event):
+            self.logger.debug("%s being published with data:\n%s" % (event.name, event.data))
             for subscriber in self.subscribers:
+                self.logger.debug("Asking %s to handle %s" % (subscriber.__class__.__name__, event.name))
                 subscriber.handle(event)
 
     instance = _EventBusImpl()
