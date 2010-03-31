7c65755fd41b64af9cd481aac62b528af6705c13
Itamar Ravid <iravid@iravid.com>
2010-04-01 01:00:44 +0300

    Starting to refactor events to be name+data records

--- event.py
+++ event.py
@@ -1,18 +1,3 @@
 class Event(object):
-    def __init__(self, data=None):
+    def __init__(self, name="Event", data=None):
         self.data = data
-
-class ControlConnectionReceived(Event):
-    pass
-
-class ControlConnectionLost(Event):
-    pass
-
-class ControlDataReceived(Event):
-    pass
-
-class MUDDataReceived(Event):
-    pass
-
-class TriggerDataGenerated(Event):
-    pass
