fe479e80ad32a599461b01a7baece1f0e38be138
Itamar Ravid <iravid@iravid.com>
2010-04-04 21:09:35 +0300

    RDesc parsing functionality

--- mxp_mapper.py
+++ mxp_mapper.py
@@ -14,18 +14,74 @@ class MXPMapper(RegexProcessor):
     break quite easily...
     """
     
+    RDESC_BUFFER_REGEX = r"^(?P<descLine>[^<]*)(?P<closeTag></RDesc>)?"
+    
     rules = {
         r"<RNum (?P<rnum>\d+)>": "FoundRnum",
-        r"<RName>(?P<rname>.*)</RName>": "FoundRname"
+        r"<RName>(?P<rname>.*)</RName>": "FoundRname",
+        r"<RDesc>(?P<descLine>[^<]*)": "FoundRdescOpen",
     }
 
     def __init__(self):
         RegexProcessor.__init__(self)
 
         self.logger = logging.getLogger("mud_proxy.MXPMapper")
+        
+        # See matchData for explanation
+        self.installLater = []
+        self.removeLater = []
+
+    def matchData(self, data):
+        responses = RegexProcessor.matchData(self, data)
+
+        # Since we don't want the rules list to mutate during a parsing pass, we schedule
+        # the addition of new rules in self.installLater and install them after we finish parsing.
+        # The RDesc and RExits bufferings use this.
+        for regex, handler in self.installLater:
+            if self.rules.get(regex):
+                self.logger.warning("Yikes! The regex I'm installing already exists...")
+
+            self.rules[regex] = handler
+
+        self.installLater = []
+
+        # Same idea, but for removing rules:
+        for regex in self.removeLater:
+            if not self.rules.get(regex):
+                self.logger.warning("Yikes! The regex I'm removing doesn't exist...")
+
+            self.rules.pop(regex, None)
+
+        self.removeLater = []
+
+        return responses
 
     def onFoundRnum(self, match):
         self.logger.info("Found RNum %s" % match.groupdict()["rnum"])
 
     def onFoundRname(self, match):
         self.logger.info("Found RName '%s'" % match.groupdict()["rname"])
+
+    def onFoundRdescOpen(self, match):
+        self.logger.info("Found RDesc opening tag, first line is: %s" % match.groupdict()["descLine"])
+        self.activateRdescBuffering(match.groupdict()["descLine"])
+
+    def activateRdescBuffering(self, firstLine):
+        self.logger.info("Activating RDesc buffering")
+        self.rdescBuffer = [firstLine,]
+        self.installLater.append((self.RDESC_BUFFER_REGEX, self.onRdescFallback))
+
+    def onRdescFallback(self, match):
+        self.logger.info("Buffering into RDesc: %s" % match.groupdict()["descLine"])
+        self.rdescBuffer.append(match.groupdict()["descLine"])
+
+        if match.groupdict().get("closeTag"):
+            self.logger.info("Found RDesc close, desc was:")
+            self.logger.info("".join(self.rdescBuffer))
+
+            # Cleanup
+            del self.rdescBuffer
+            self.removeLater.append(self.RDESC_BUFFER_REGEX)
+
+    def onFallback(self, match):
+        self.logger.info("Fallback matched: %s" % match.groups()[0])
