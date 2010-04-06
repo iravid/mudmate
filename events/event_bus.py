1d71a4fa6e8cb94d7fc7a2fe95a08752c9392539
Itamar Ravid <iravid@iravid.com>
2010-04-07 00:22:01 +0300

    Refactored MUDClient to inherit from twisted.conch.telnet.Telnet, ridding us of the need to have that huge state-machine in TelnetStripper which will be promptly renamed/refactored.
    Also, dropped some really spamming logger.debug calls in EventBus and RegexProcessor.

--- events/event_bus.py
+++ events/event_bus.py
@@ -7,15 +7,11 @@ class EventBus(object):
             self.logger = logging.getLogger("mud_proxy.EventBus")
 
         def subscribe(self, subscriber):
-            self.logger.debug("%s subscribing" % subscriber.__class__.__name__)
             if subscriber not in self.subscribers:
                 self.subscribers.append(subscriber)
 
         def publish(self, event):
-            self.logger.debug("%s being published with data:\n%s" % (event.name, event.data))
-
             for subscriber in self.subscribers:
-                self.logger.debug("Asking %s to handle %s" % (subscriber.__class__.__name__, event.name))
                 subscriber.handle(event)
 
     instance = _EventBusImpl()
